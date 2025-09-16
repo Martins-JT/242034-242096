4bda8daf927449274e694f4d7efac4991af9de5b
Martins-JT <jteixeirabr42@gmail.com>
2025-09-15 21:58:06 -0300

    15/09/2025

--- 242034 242096/Views/FrmCaixa.cs
+++ 242034 242096/Views/FrmCaixa.cs
@@ -29,7 +29,7 @@ namespace _242034_242096.Views
         {
             if (troco < 0)
             {
-                MessageBox.Show("", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Valor insuficiente", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                 return;
             }
